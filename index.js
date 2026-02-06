4d7ea510c32820515a1062058f0321dc100bf1a2
Nitesh Chavan <chavannitesh963@gmail.com>
2026-02-07 00:16:24 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=> {
+    res.send("Nitesh Chavan")
+})
+
+app.get("/login",(req,res)=> {
+    res.send("<h1>Please login first</h1>")
+})
+
+app.get("/backend", (req,res)=> {
+    res.send(`<ul>
+        <li>Node.js</li>
+        <li>MongoDB</li>
+        <li>Express.js</li>
+        </ul>`)
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port http://localhost:${process.env.PORT}`)
+})
